4ae8e2a3fb73747f5318df7eac6e668cfdc38de8
victor.salaun <victor.salaun@gmail.com>
2017-11-09 19:52:02 +0100

    make personne vars nullable

--- src/models/Personne.js
+++ src/models/Personne.js
@@ -4,16 +4,16 @@ import { types } from "mobx-state-tree";
 const Personne = types
   .model("Personne", {
     id: types.identifier(types.number),
-    nom: types.optional(types.string, ""),
-    prenom: types.optional(types.string, ""),
-    date_de_naissance: types.optional(types.string, ""),
-    photo: types.optional(types.string, ""),
-    mail_pro: types.optional(types.string, ""),
-    mail_perso: types.optional(types.string, ""),
-    tel_pro: types.optional(types.string, ""),
-    tel_perso: types.optional(types.string, ""),
-    poste: types.optional(types.string, ""),
-    description_libre: types.optional(types.string, "")
+    nom: types.maybe(types.string),
+    prenom: types.maybe(types.string),
+    date_de_naissance: types.maybe(types.string),
+    photo: types.maybe(types.string),
+    mail_pro: types.maybe(types.string),
+    mail_perso: types.maybe(types.string),
+    tel_pro: types.maybe(types.string),
+    tel_perso: types.maybe(types.string),
+    poste: types.maybe(types.string),
+    description_libre: types.maybe(types.string)
   })
   .actions(self => ({
     afterCreate: () => {
